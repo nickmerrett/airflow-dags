d8f4fbd26c8a197d534ff2f1c3aa2103960b5d86
nickmerrett <88360368+nickmerrett@users.noreply.github.com>
2024-09-18 09:28:59 +1000

    Update databand_airflow_monitor.py remove cert hack

--- airflow/dags/databand_airflow_monitor.py
+++ airflow/dags/databand_airflow_monitor.py
@@ -1,7 +1,4 @@
 from airflow_monitor.monitor_as_dag import get_monitor_dag
 ## This DAG is used by Databand to monitor your Airflow installation.
 
-dag = get_monitor_dag(check_interval=2, monitor_env={"CURL_CA_BUNDLE": ""})
-
-if __name__ == "__main__":
-    dag.cli()
+dag = get_monitor_dag()
